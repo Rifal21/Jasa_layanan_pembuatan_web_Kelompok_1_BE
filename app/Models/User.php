2941c64fc00e9861f2c864bb457e338eff820513
Rifal21 <giantjou77@gmail.com>
2023-05-29 08:47:45 +0700

    integrate database

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,8 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      *
      * @var string[]
      */
-    protected $fillable = [
-        'name', 'email',
-    ];
+    protected $table = 'users';
+    protected $guarded = 'id';
 
     /**
      * The attributes excluded from the model's JSON form.
@@ -30,4 +29,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
     protected $hidden = [
         'password',
     ];
+
+
+
 }
